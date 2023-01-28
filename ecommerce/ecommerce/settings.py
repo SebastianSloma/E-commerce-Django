ab91fc4833e5278ea2621c9154a3bfb9ee8b54ce
Sebastian <sebastiansloma@gmail.com>
2023-01-28 18:20:15 +0100

    .

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6ax$gt(i%ffj^bces=^$k(z7abbogd+eek_ew46nmpfbolnq@1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['storedjango1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
