71a298ed16453359862eeb6101ad98f557b04fa2
Sebastian <sebastiansloma@gmail.com>
2023-01-28 18:44:07 +0100

    .

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6ax$gt(i%ffj^bces=^$k(z7abbogd+eek_ew46nmpfbolnq@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['storedjango1.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
