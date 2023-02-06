04b35785a92d6db27ac10749fd87ce478cb47469
Sebastian <sebastiansloma@gmail.com>
2023-02-06 16:00:02 +0100

    .

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6ax$gt(i%ffj^bces=^$k(z7abbogd+eek_ew46nmpfbolnq@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
